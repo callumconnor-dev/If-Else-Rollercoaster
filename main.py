6aa7b74fd1f1ba54c09125221f92642d553124dd
callumconnor-dev <132172088+callumconnor-dev@users.noreply.github.com>
2023-07-10 23:02:58 +0100

    Rollercoaster Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Rollercoaster!")
+height = int(input("How tall are you, in cm? "))
+
+if height >= 180:
+    print(f"You are {height}cm tall; enjoy the ride")
+else:
+    print(f"You are {height}cm tall; you are not permitted to ride the Rollercoaster, have a nice day.")
