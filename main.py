a06cc2c27b53040a856839933f88cf3434bb1277
callumconnor-dev <132172088+callumconnor-dev@users.noreply.github.com>
2023-07-11 01:00:08 +0100

    Ticket Update

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ height = int(input("How tall are you, in cm? "))
 
 if height >= 120:
   age = int(input(f"You are {height}cm tall; how old are you? "))
-  
-  if age >= 18:
-    print(f"You are {age}; The ticket is £14, enjoy the ride.")
+  photo = input(f"Would you like a £3 photo ticket? Yes or No ")
+  if age >= 18: 
+    ticketPrice = 12
   elif age >= 12:
-    print(f"You are {age}; The ticket is £8, enjoy the ride.")
-  else:
-    print(f"You are {age}; You must be 12 or older to ride this ride.")
-    exit()
+    ticketPrice = 10
+  elif age >= 8:
+    ticketPrice = 8
+  
+  if photo == "Yes":
+    ticketPrice += 3
+  
+  print(f"You are {age}; The ticket is £{ticketPrice}, enjoy the ride.")
   
 else:
   print(f"You are {height}cm tall; you are not permitted to ride the Rollercoaster, have a nice day.")
